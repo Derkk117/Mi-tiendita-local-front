b3a41148506ede12579daa32e45f23bb8d517048
Moymon <mo.goto@hotmail.com>
2021-03-23 20:41:58 -0600

    module-suppliers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/Layout.module';
 import { DashboardComponent } from './modules/home/pages/dashboard/dashboard/dashboard.component';
+//import { SuppliersCreateComponent } from './modules/home/pages/suppliers/suppliers-create/suppliers-create.component';
+//import { SuppliersIndexComponent } from './modules/home/pages/suppliers/suppliers-index/suppliers-index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    //SuppliersCreateComponent,
+    //SuppliersIndexComponent,
   ],
   imports: [
     BrowserModule,
