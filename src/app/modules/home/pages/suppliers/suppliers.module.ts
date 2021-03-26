5ead871108341eab8d7cf6b1985ebb81855b9705
Derkk117 <derkk117@gmail.com>
2021-03-25 21:35:57 -0600

    nothing

--- src/app/modules/home/pages/suppliers/suppliers.module.ts
+++ src/app/modules/home/pages/suppliers/suppliers.module.ts
@@ -9,20 +9,19 @@ import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCheckboxModule} from '@angular/material/checkbox';
-import { MatButtonModule} from '@angular/material/button';
-import {FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatListModule } from '@angular/material/list';
-import { MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { SuppliersEditComponent } from './suppliers-edit/suppliers-edit.component';
-
 
 @NgModule({
   declarations: [
     SuppliersCreateComponent,
     SuppliersIndexComponent,
     SuppliersEditComponent
-],
+  ],
   imports: [
     CommonModule,
     SuppliersRoutingModule,
