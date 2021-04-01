e220ec5e2ed6f7a835d1d77cfd8661443aa1d2f0
Berenice <BereniceHdz>
2021-03-31 20:12:27 -0600

    Funciona correctamente el sideNav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 /* MODULOS */
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from './layout/Layout.module';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 /* COMPONENTES */
 import { AppComponent } from './app.component';
@@ -18,12 +26,19 @@ import { SideBarComponent } from './modules/home/components/side-bar/side-bar.co
     SideBarComponent,
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,    
     AppRoutingModule, 
-
-    RouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,        
     LayoutModule,
+    MatButtonModule,    
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/components/side-bar/side-bar.component.ts
+++ src/app/modules/home/components/side-bar/side-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 
-export class SideBarComponent implements OnInit{
+export class SideBarComponent implements OnDestroy{
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
 
-  ngOnInit(): void {  
+  fillerNav = [
+    {name:"Ventas", route:"Ventas", icon:"receipt"},
+    {name:"Articulos", route:"Articulos", icon:"shopping_cart"},
+    {name:"Inventarios", route:"Inventarios", icon:"app_registration"},
+    {name:"Clientes", route:"Clientes", icon:"supervisor_account"},
+    {name:"Proveedores", route:"Proveedores", icon:"supervised_user_circle"},
+    {name:"Cortes", route:"Cortes", icon:"shuffle"},
+    {name:"Entregas", route:"Entregas", icon:"airport_shuttle"},
+    {name:"Configuracion", route:"Configuracion", icon:"account_circle"},
+  ]
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router:Router) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+    
+  shouldRun = true;
+
+  navegar(url:string)
+  {
+    this.router.navigate([url]);
   }
 }
