fe55782809180c8f8a73bc1184a6e7ce3bd88ae8
Derkk117 <derkk117@gmail.com>
2021-04-04 23:29:32 -0500

    bars v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from './layout/Layout.module';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './modules/home/components/top-bar/top-bar.component';
-import { SideBarComponent } from './modules/home/components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent,
-    TopBarComponent
   ],
   imports: [
-    AppRoutingModule, 
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,        
+    HttpClientModule,
     LayoutModule,
-    MatButtonModule,    
+    MatButtonModule,
     MatIconModule,
     MatMenuModule,
     MatToolbarModule,
