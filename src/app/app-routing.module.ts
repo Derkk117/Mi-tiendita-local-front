55a4e680aa7258ce90370220765da2a6906a53d5
Berenice <BereniceHdz>
2021-04-13 00:37:07 -0500

    Se realizo login y register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { NoAuthGuard } from './core/guards/no-auth.guard';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import {LoginComponent} from './modules/auth/pages/login/login.component';
+import {RegisterComponent} from './modules/auth/pages/register/register.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -26,6 +28,9 @@ const routes: Routes = [
 			},
 		]
 	},
+	{ path: 'login', component: LoginComponent},
+	{ path: 'register', component: RegisterComponent},
+	
 	{
 		path: 'auth',
 		component: AuthLayoutComponent,
