0ecbbd7ff54319ae181c1ad524fd13672f0c67b5
Derkk117 <derkk117@gmail.com>
2021-05-25 22:58:31 -0500

    merge

--- src/app/modules/home/pages/sales/sales-create/sales-create.component.ts
+++ src/app/modules/home/pages/sales/sales-create/sales-create.component.ts
@@ -6,52 +6,7 @@ import { Sale } from 'src/app/shared/models/Sale_model';
 @Component({
   selector: 'app-sales-create',
   templateUrl: './sales-create.component.html',
-  styles: [`
-  .outer{
-    height:200px;
-    line-height: 200px;
-  }
-
-  .container-form{
-    width: 50%;
-    margin: 1rem auto;
-    box-shadow: -1px 3px 66px 0px rgba(92,158,173,1);
-    padding: 1rem;
-    border-radius: 10px;
-  }
-
-  select{
-    border-radius: 10px;
-    width:75%;
-    text-align: center;
-  }
-
-  mat-form-field{
-    font-size: 14px;
-    width: 100%;
-    padding: 1rem;
-    margin: 1rem auto;
-    border: 1px;
-    border-radius: 10px;
-  }
-
-  input{
-    border-radius: 10px;
-    width: 75%;
-    text-align: center;
-  }
-
-  textarea{
-    border-radius: 10px;
-    width: 75%;
-    text-align: center;
-  }
-
-  .mat-button{
-    color: #FFFFFF;
-    background-color: #326273;
-  }
-  `]
+  styleUrls: 
 })
 export class SalesCreateComponent implements OnInit {
 
