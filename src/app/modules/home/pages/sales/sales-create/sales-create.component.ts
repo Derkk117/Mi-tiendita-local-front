13e275aabce115c62c61bafbe4ac9e00a717858c
Derkk117 <derkk117@gmail.com>
2021-05-25 23:16:08 -0500

    form v1 for sale store

--- src/app/modules/home/pages/sales/sales-create/sales-create.component.ts
+++ src/app/modules/home/pages/sales/sales-create/sales-create.component.ts
@@ -1,30 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators, FormGroup} from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Sale } from 'src/app/shared/models/Sale_model';
+import { ClientService } from 'src/app/shared/services/Client_service';
 
 @Component({
   selector: 'app-sales-create',
   templateUrl: './sales-create.component.html',
-  styleUrls: 
+  styleUrls: ['./sales-create.component.scss'],
+  providers: [
+    ClientService
+  ]
 })
 export class SalesCreateComponent implements OnInit {
+  clients: any;
+  token;
+  identity;
+  sale;
 
-
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private _clientService: ClientService
+  ) { }
 
   ngOnInit(): void {
+    this.sale = new Sale("", null, "", "", "", "", new Date());
+    this.identity = JSON.parse(localStorage.getItem('identity'));
+    this.token = localStorage.getItem('session');
+    this._clientService.getClients(this.token, this.identity.id).subscribe(response => {
+      this.clients = response;
+
+      this.clients.forEach(element => {
+        element['fullName'] = element['name'] + " " + element['last_name'];
+      });
+    },
+      error => {
+        console.log(error);
+      });
   }
-  
-  addregreso(){    
+
+  addregreso() {
     this.router.navigate(['sales/']);
   }
 
-  ingresaSales(){
+  ingresaSales() {
   }
-  lista:string[]=["CASH", "CARD"];
+  lista: string[] = ["CASH", "CARD"];
   seleccionado = "";
-  
-   
+
+
 }
 
